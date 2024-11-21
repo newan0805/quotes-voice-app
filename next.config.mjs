75f331c30dd97c36d42fc2a6c13e9f555de38d33
Newan Kumanayaka <newan0805@gmail.com>
2024-11-21 17:02:40 +0530

    fixed the quotes issue was on the QuoteCard

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
-// next.config.mjs
-import withPWA from "next-pwa";
+// // next.config.mjs
+// import withPWA from "next-pwa";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the service worker
-  skipWaiting: true, // Enable skip waiting for immediate updates
-});
+// /** @type {import('next').NextConfig} */
+// const nextConfig = withPWA({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+//   register: true, // Register the service worker
+//   skipWaiting: true, // Enable skip waiting for immediate updates
+// });
+
+// export default nextConfig;
+
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default nextConfig;
--- src/app/components/QuoteCard.jsx
+++ src/app/components/QuoteCard.jsx
@@ -5,7 +5,7 @@ export default function QuoteCard({ quote, audioUrl }) {
   return (
     <div className="bg-white p-4 rounded-lg shadow-md mb-4">
       <audio controls src={audioUrl} className="w-full mb-2"></audio>
-      <p className="text-gray-800">"{quote}"</p>
+      <p className="text-gray-800">&quot;{quote}&quot;</p>
     </div>
   );
 }
